a43cba1e7bb0674e3a08a98f7302f59e412287a6
Hirakari Akshitha <hirakariakshitha@gmail.com>
2025-12-12 22:01:20 +0530

    Add files via upload

---
+++ KeywordArgex1.py
@@ -0,0 +1,10 @@
+#keywordArgsex1.py
+def disp(a,b,c,d):
+    print("\t{}\t{}\t{}\t{}".format(a,b,c,d))
+#main program
+print("-"*50)
+print("\tA\tB\tC\tD")
+print("-"*50)
+disp(10,20,30,40)
+disp(d=40,a=10,c=30,b=32)
+disp(10,20,d=40,c=12)
