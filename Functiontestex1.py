3a075a9f96178f5c2bc2e867c636447feb78e9de
Hirakari Akshitha <hirakariakshitha@gmail.com>
2025-12-12 21:59:31 +0530

    Add files via upload

---
+++ Functiontestex1.py
@@ -0,0 +1,7 @@
+#functiontestex1.py
+print("iam brfore  the function defination")
+def welcome():
+    print("\t\twelcome to functional programming")
+print("am after the function defination")
+welcome()
+print("am after the function call")
